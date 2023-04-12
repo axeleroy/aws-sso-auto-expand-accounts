80a84d151ee81d8fec2dbc160da49164212e7b33
Axel Leroy <axel.leroy@glady.com>
2023-04-12 11:40:22 +0200

    feat: auto-expand accounts in filtered list (#1)

--- auto-expand.js
+++ auto-expand.js
@@ -12,15 +12,22 @@ new MutationObserver((mutations) => {
     const accountApplicationNodes = addedNodes
     .filter((node) => node.tagName === 'PORTAL-APPLICATION'
         && node.getAttribute('title') === 'AWS Account');
+
     const accountsSections = addedNodes
-    .filter((node) => node.tagName === 'SSO-EXPANDER')
-    .map((node) => node.getElementsByClassName('instance-section'));
+    .filter((node) => node.tagName === 'SSO-EXPANDER'
+        || node.tagName == 'SSO-SEARCH-RESULT-LIST')
+    .map((node) => node.getElementsByClassName('instance-section'))
+    // Flatten HTMLCollection to Node[]
+    .reduce((previousValue, currentValue) =>
+        previousValue.concat(Array.from(currentValue)), [])
+    // Filter on sections that have an expander child
+    .filter((instanceSection) => instanceSection.querySelectorAll('.expandIcon').length > 0);
 
     if (accountApplicationNodes.length > 0) {
         accountApplicationNodes[0].click();
     }
     if (accountsSections.length > 0) {
-        Array.from(accountsSections[0]).forEach((section) => section.click());
+        Array.from(accountsSections).forEach((section) => section.click());
     }
 })
 .observe(document.body, { subtree: true, childList: true });
